a377071c961ad588158f23609e02f570aa7f52c6
Blake Dozler <blake.dozler@pcc.edu>
2017-05-02 12:22:19 -0700

    added feature

--- src/cis233j/helloworld/HelloWorld.java
+++ src/cis233j/helloworld/HelloWorld.java
@@ -4,4 +4,8 @@ package cis233j.helloworld;
  * Created by Blake Dozler on 5/1/2017.
  */
 public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
 }
