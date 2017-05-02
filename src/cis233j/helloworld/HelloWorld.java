82a80fc72bb7290d15ae466dfe5d14444e1a25d9
Blake Dozler <blake.dozler@pcc.edu>
2017-05-02 12:25:19 -0700

    print date

--- src/cis233j/helloworld/HelloWorld.java
+++ src/cis233j/helloworld/HelloWorld.java
@@ -1,4 +1,5 @@
 package cis233j.helloworld;
+import java.util.Date;
 
 /**
  * Created by Blake Dozler on 5/1/2017.
@@ -7,5 +8,6 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        System.out.println(new Date());
     }
 }
